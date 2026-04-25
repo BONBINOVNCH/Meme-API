f524f6ad6055329f8956fe412b670d1e231abf98
Max <zmax1301@gmail.com>
2026-04-25 16:44:38 +0200

    baze_getting_data

--- public/index.js
+++ public/index.js
@@ -1,6 +1,15 @@
+const memes_block = document.querySelector(".main_content_block");
+
 async function getData() {
     const data = await axios("/api/memes");
     console.log(data);
+    const memes = data.data;
+    console.log(memes);
+    for (const meme of memes) {
+        memes_block.innerHTML += `
+        <div>${meme.title}</div>
+    `;
+    }
 }
 
 getData();
