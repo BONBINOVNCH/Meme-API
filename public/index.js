b7a2c7c4cb706cdbd2c59815440c9aef4c9ccc80
Max <zmax1301@gmail.com>
2026-04-25 17:54:26 +0200

    getting_tempaltes

--- public/index.js
+++ public/index.js
@@ -1,10 +1,17 @@
+import convertImg from "./utils/convert.js";
+
 const memes_block = document.querySelector(".main_content_block");
+const input_file = document.querySelector(".file");
 
 async function getData() {
     const data = await axios("/api/memes");
+    const data_templates = await axios("/api/templates");
     console.log(data);
+    console.log(data_templates);
     const memes = data.data;
+    const templates = data_templates.data;
     console.log(memes);
+    console.log(templates);
     for (const meme of memes) {
         memes_block.innerHTML += `
         <div>${meme.title}</div>
@@ -13,3 +20,35 @@ async function getData() {
 }
 
 getData();
+
+const memeImages = {
+    "This is fine": "https://i.imgflip.com/26am.jpg",
+    "Works on my machine": "https://i.imgflip.com/1w7ygt.jpg",
+    "Friday deploy": "https://i.imgflip.com/30b1gx.jpg",
+    "Student mode": "https://i.imgflip.com/2fm6x.jpg",
+};
+
+input_file.addEventListener("change", async (e) => {
+    const currFiles = e.target.files;
+
+    if (currFiles.length > 0) {
+        let src = URL.createObjectURL(currFiles[0]);
+        const base64Img = await convertImg(currFiles[0]);
+        console.log(base64Img);
+
+        const template = {
+            name: "Student mode",
+            imageBase64: base64Img,
+            mimeType: "image/png",
+            width: 180,
+            height: 180,
+        };
+
+        try {
+            const response = await axios.post("/api/templates", template);
+            console.log(response);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+});
--- public/utils/convert.js
+++ public/utils/convert.js
@@ -1,4 +1,4 @@
-function convertImg(file, maxHeight = 180, maxWidth = 180, quality = 0.9) {
+function convertImg(file, maxHeight = 512, maxWidth = 512, quality = 0.9) {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
         reader.readAsDataURL(file);
