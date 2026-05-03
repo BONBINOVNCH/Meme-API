f5d098e3cb9f8fa6601144742ded4f782a7c9cc2
Max <zmax1301@gmail.com>
2026-05-03 15:01:04 +0200

    full_functionality

--- public/scripts/constructor.js
+++ public/scripts/constructor.js
@@ -6,13 +6,16 @@ const meme_title = document.querySelector(".meme_title");
 const meme_response = document.querySelector(".meme_response");
 const meme_category = document.querySelector(".meme_category");
 
+const post_btn = document.querySelector(".post");
+
 async function getData(choice = "first") {
     choose_template_line.innerHTML = ``;
     const data_templates = await axios("/api/templates");
     const templates = data_templates.data;
+    console.log(templates);
     for (const template of templates) {
         choose_template_line.innerHTML += `
-             <div class="template_item">
+             <div class="template_item" id="${template.name}">
         <div class="template_image_wrapper">
             <img src="${template.imageBase64}" alt="${template.name}" />
         </div>
@@ -26,8 +29,10 @@ async function getData(choice = "first") {
     console.log(items);
     if (choice === "first") {
         items[0].classList.add("active");
+        meme_title.textContent = items[0].id;
     } else {
         items[items.length - 1].classList.add("active");
+        meme_title.textContent = items[items.length - 1].id;
     }
     const item_img = document.querySelector(".active img");
     card_img.src = item_img.src;
@@ -46,6 +51,8 @@ document.addEventListener("click", (e) => {
     item.classList.add("active");
     const item_img = document.querySelector(".active img");
     card_img.src = item_img.src;
+
+    meme_title.textContent = item.id;
 });
 
 let template = {
@@ -65,12 +72,12 @@ const template_category = document.querySelector(
     ".choose_template_category_input",
 );
 
-template_name.addEventListener("input", (e) => {
-    meme_title.textContent = e.target.value;
-    if (!e.target.value) {
-        meme_title.textContent = "Титул шаблону";
-    }
-});
+// template_name.addEventListener("input", (e) => {
+//     meme_title.textContent = e.target.value;
+//     if (!e.target.value) {
+//         meme_title.textContent = "Титул шаблону";
+//     }
+// });
 
 template_text.addEventListener("input", (e) => {
     meme_response.textContent = e.target.value;
@@ -118,5 +125,21 @@ btn_download.addEventListener("click", async (e) => {
 
             getData("last");
         }
+    }
+});
+
+post_btn.addEventListener("click", async () => {
+    let meme = {
+        title: meme_title.textContent,
+        category: meme_category.textContent,
+        response: meme_response.textContent,
+        tags: [],
+    };
+
+    try {
+        const response = await axios.post("/api/memes", meme);
+        console.log(response);
+    } catch (e) {
+        console.log(e);
     }
 });
